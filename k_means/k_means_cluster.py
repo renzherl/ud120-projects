7458bc73ff893ec9749e9675bc71d510ff5492ed
renzherl <renzherl@126.com>
2018-09-17 15:37:38 +0800

    k_means

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -7,7 +7,7 @@
 
 
 
-import pickle
+import pprint,pickle
 import numpy
 import matplotlib.pyplot as plt
 import sys
@@ -42,31 +42,42 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
+#pprint.pprint(data_dict)
 
 
 ### the input features we want to use 
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
+features_stock = [feature_2]
+stock = featureFormat(data_dict, features_stock)
+print(max(stock))
+print(min(stock))
+
+features_salary = [feature_1]
+salary = featureFormat(data_dict, features_salary)
+print(max(salary))
+print(min(salary))
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, _ in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
-
-
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=2, random_state=0).fit(data)
+pred = kmeans.labels_
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
